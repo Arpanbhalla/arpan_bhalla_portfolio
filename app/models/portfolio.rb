adfeb2b3fec0cf2e4eed11ee660a5cac77e3aebf
Arpan Bhalla <arpanbhalla@gmail.com>
2018-07-12 16:09:29 +1000

    topic | technology

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,2 +1,16 @@
 class Portfolio < ApplicationRecord
+  validates_presence_of :title, :subtitle, :body, :main_image
+  include Placeholder
+  has_many :technologies
+  accepts_nested_attributes_for :technologies,
+                                           reject_if: lambda { |attr| attr['name'].blank? }
+
+  scope :react_scope, -> { where(subtitle: 'React') }
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
+    self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,11 @@
+3.times do |topic|
+  Topic.create!(
+      title: "Title #{rand(1..3)}"
+  )
+end
+
+puts "#{Topic.count} topics created."
+
 10.times do |blog|
   Blog.create!(
           title: "Blog title number #{blog + 1}",
@@ -9,7 +17,8 @@
                  reprehenderit in voluptate velit esse cillum
                  dolore eu fugiat nulla pariatur.
                  Excepteur sint occaecat cupidatat non proident,
-                 sunt in culpa qui officia deserunt mollit anim id est laborum."
+                 sunt in culpa qui officia deserunt mollit anim id est laborum.",
+          topic_id: "#{rand(1..3)}"
           )
 end
 
@@ -24,10 +33,24 @@ end
 
 puts "#{Skill.count} skills created."
 
-9.times do |portfolio|
+8.times do |portfolio|
   Portfolio.create!(
-      title: "Portfolio no. #{portfolio + 1}",
-      subtitle: "Subtitle no. #{portfolio + 1}",
+      title: "Title #{portfolio + 1}",
+      subtitle: "Ruby on rails",
+      body: "Lorem ipsum dolor sit amet, consectetur adipiscing
+             elit, sed do eiusmod tempor incididunt ut labore et
+             dolore magna aliqua. Ut enim ad minim veniam, quis
+             nostrud exercitation ullamco laboris nisi ut aliquip
+             ex ea commodo consequat. Duis aute irure dolor in",
+      main_image: "http://via.placeholder.com/600x400",
+      thumb_image: "http://via.placeholder.com/350x200"
+  )
+end
+
+1.times do |portfolio|
+  Portfolio.create!(
+      title: "Title #{portfolio + 1}",
+      subtitle: "React",
       body: "Lorem ipsum dolor sit amet, consectetur adipiscing
              elit, sed do eiusmod tempor incididunt ut labore et
              dolore magna aliqua. Ut enim ad minim veniam, quis
@@ -39,3 +62,12 @@ puts "#{Skill.count} skills created."
 end
 
 puts "#{Portfolio.count} Portfolio created."
+
+3.times do |tech|
+  Technology.create!(
+      name: "Technology - #{ tech + 1 }",
+      portfolio_id: "#{rand(1..3)}"
+  )
+end
+
+puts "#{Technology.count} Technologies created."
