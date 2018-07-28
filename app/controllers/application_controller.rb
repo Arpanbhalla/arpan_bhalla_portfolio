45d0105130d6f9fb2d9636c65fdb172dccb6155f
Pratima ARPAN <pratimaarpan@Pratimas-iMac-7.local>
2018-07-29 04:57:47 +1000

    implemeted copy right

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,21 @@ class ApplicationController < ActionController::Base
   include DeviseWhitelist
   include PortfolioTitle
   protect_from_forgery with: :exception
+  before_action :set_copyright
+
+  def set_copyright
+    @copyright = ABViewTool::Renderer.copyright 'Arpan Bhalla', 'All rights reserved.'
+  end
 
   def current_user
     super || OpenStruct.new(first_name: 'Guest')
+  end
+
+  module ABViewTool
+    class Renderer
+      def self.copyright name, msg
+        "&copy;#{Time.now.year} | <b>#{name}<b> #{msg}".html_safe
+      end
+    end
   end
 end
